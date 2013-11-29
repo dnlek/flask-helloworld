df0ef6943f6b993889fea197cf2abe69c727fc4c
Daniel Wagner <dnl.wagner@gmail.com>
2013-11-29 13:04:44 +0100

    wsparcie dla mongodb

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from flask import Flask
 from flask_testing import TestCase
 from app import create_app
@@ -12,6 +13,17 @@ class FlaskrTestCase(TestCase):
         rv = self.client.get('/')
         assert rv.status == '200 OK'
         assert 'Hello World!' in rv.data
+
+    def test_tasks(self):
+        t = self.client.application.db.Task()
+        t.title = u"test"
+        t.save()
+        tasks = self.client.application.db.Task.find()
+
+        rv = self.client.get('/tasks')
+        d = json.loads(rv.data)
+        assert len(d) == tasks.count()
+
 
 if __name__ == '__main__':
     unittest.main()
